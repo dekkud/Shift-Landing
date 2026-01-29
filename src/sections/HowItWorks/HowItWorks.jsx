ff66fddd7b77c4024c96b552360be433c79f06bf
dekud <lispain666@gmail.com>
2026-01-29 18:57:16 +0100

    add new UI element

--- src/sections/HowItWorks/HowItWorks.jsx
+++ src/sections/HowItWorks/HowItWorks.jsx
@@ -1,7 +1,9 @@
 import styles from './HowItWorks.module.css'
 import howItWorksStepsImage from '@/assets/png/howItWorks.png'
+import steps from '@/config/steps'
 
 const HowItWorks = () => {
+
 	return (
 		<section className={styles.howItWorks}>
 			<div className={styles.container}>
@@ -12,7 +14,19 @@ const HowItWorks = () => {
 				</div>
 
 				<div className={styles.subStepsContainer}>
-					<div className={styles.stepsContainer}></div>
+					<div className={styles.stepsContainer}>
+						{steps.map((step, index) => (
+							<div className={styles.stepItems} key={index}>
+								<div className={styles.subStepNumber}>
+									<div className={styles.stepNumber}>{index + 1}</div>
+								</div>
+								<div className={styles.stepInfo}>
+									<h1>{step.title}</h1>
+									<p>{step.text}</p>
+								</div>
+							</div>
+						))}
+					</div>
 					<div className={styles.imgContainer}>
 						<img src={howItWorksStepsImage} alt="steps" className={styles.stepsImg} />
 					</div>
